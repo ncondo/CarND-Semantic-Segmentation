647532a8ff48ca8fd663a13f268abf1f0f58d886
Nicholas Condo <nicholas.condo@gmail.com>
2017-08-01 03:50:13 -0400

    feat: Implement load_vgg for loading pre-trained vgg16 model and returning significant layers for deconvolutions

--- main.py
+++ main.py
@@ -27,13 +27,15 @@ def load_vgg(sess, vgg_path):
     # TODO: Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
-    vgg_input_tensor_name = 'image_input:0'
-    vgg_keep_prob_tensor_name = 'keep_prob:0'
-    vgg_layer3_out_tensor_name = 'layer3_out:0'
-    vgg_layer4_out_tensor_name = 'layer4_out:0'
-    vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    vgg_input_tensor_name = graph.get_tensor_by_name('image_input:0')
+    vgg_keep_prob_tensor_name = graph.get_tensor_by_name('keep_prob:0')
+    vgg_layer3_out_tensor_name = graph.get_tensor_by_name('layer3_out:0')
+    vgg_layer4_out_tensor_name = graph.get_tensor_by_name('layer4_out:0')
+    vgg_layer7_out_tensor_name = graph.get_tensor_by_name('layer7_out:0')
+
+    return vgg_input_tensor_name, vgg_keep_prob_tensor_name, vgg_layer3_out_tensor_name, vgg_layer4_out_tensor_name, vgg_layer7_out_tensor_name
 tests.test_load_vgg(load_vgg, tf)
 
 
