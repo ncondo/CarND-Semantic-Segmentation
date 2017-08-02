a08e41d4b2d44ce1d5d61578001829648bc43217
Nicholas Condo <nicholas.condo@gmail.com>
2017-08-01 21:17:54 -0400

    feat: Define loss function and optimizer for training network

--- main.py
+++ main.py
@@ -81,7 +81,15 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    # Reshape last layer of network and labels to 2D tensors
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    labels = tf.reshape(correct_label, (-1, num_classes))
+    # Define loss function as cross entropy loss
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+    # Define optimizer as Adam
+    train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
